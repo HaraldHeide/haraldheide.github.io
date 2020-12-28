09808c1932ec86311a975976614332659c84279f
Harald Heide Gundersen <harald.heide.gundersen@hotmail.com>
2020-12-28 17:37:35 +0100

    nyscale

--- tietoevry/app.js
+++ tietoevry/app.js
@@ -140,9 +140,9 @@ class App{
     }
     
 	render( ) {   
-        this.chair.scaleX( 500.0 );
-        this.chair.scaleY( 500.0 );
-        this.chair.scaleZ( 500.0 );
+        this.chair.scaleX( 100.0 );
+        this.chair.scaleY( 100.0 );
+        this.chair.scaleZ( 100.0 );
         this.chair.rotateY( 0.01 );
         this.renderer.render( this.scene, this.camera );
     }
